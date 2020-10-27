f937502c3f0da51245abf14a4e92ac541dbf948a
Shekhar Arya <9925648129shekhar@gmail.com>
2020-10-27 15:28:20 +0530

    C# Basic files added

---
+++ hello.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace hello
+{
+    class Hello
+    {
+        public static void Main(String[] args){
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ programStructure.cs
@@ -0,0 +1,20 @@
+/*A C# program consists of the following parts −
+
+Namespace declaration
+A class
+Class methods
+Class attributes
+A Main method
+Statements and Expressions
+Comments*/
+
+using System;
+namespace programStructure
+{
+    public class programStructure{
+        public static void Main(String[] args)
+        {
+            System.Console.WriteLine("Hello Shekhar");
+        }
+    }
+}
